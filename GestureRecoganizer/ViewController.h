7a27f7bdba6e26f383efb354720fa2e8bef0a95e
user <androiduser@Androidusers-Mac-mini.local>
2012-10-17 18:04:49 +0530

    Initial Commit

---
+++ GestureRecoganizer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GestureRecoganizer
+//
+//  Created by Androiduser on 17/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
